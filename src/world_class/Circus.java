f1a52384cb2396ad127d01b1d79b72b957b35f3c
Abdelfattah Mohamed <abdelftahmohamed30@gmail.com>
2018-12-17 23:45:06 +0200

    Intersect

--- src/world_class/Circus.java
+++ src/world_class/Circus.java
@@ -10,90 +10,93 @@ import java.util.List;
 
 public class Circus implements World {
 
-    private static int MAX_TIME = 1 * 60 * 1000;	// 1 minute
-    private static int RIGHT_ROCKET = 123456;
-    private static int LEFT_ROCKET  = 654321;
-    private int score = 0;
-    private boolean fireRocket = false;
-    private long startTime = System.currentTimeMillis();
-    private final int width;
-    private final int height;
-    private Strategy difficulty;
-    private final List<GameObject> constant = new LinkedList<GameObject>();
-    private final List<GameObject> moving = new LinkedList<GameObject>();
-    private final List<GameObject> control = new LinkedList<GameObject>();
+	private static int MAX_TIME = 1 * 60 * 1000; // 1 minute
+	private static int RIGHT_ROCKET = 123456;
+	private static int LEFT_ROCKET = 654321;
+	private int score = 0;
+	private boolean fireRocket = false;
+	private long startTime = System.currentTimeMillis();
+	private final int width;
+	private final int height;
+	private Strategy difficulty;
+	private final List<GameObject> constant = new LinkedList<GameObject>();
+	private final List<GameObject> moving = new LinkedList<GameObject>();
+	private final List<GameObject> control = new LinkedList<GameObject>();
 
-    public Circus(int width, int height, Strategy difficulty) {
-        this.width = width;
-        this.height = height;
-        this.difficulty = difficulty;
-    }
+	public Circus(int width, int height, Strategy difficulty) {
+		this.width = width;
+		this.height = height;
+		this.difficulty = difficulty;
+	}
 
-    @Override
-    public List<GameObject> getConstantObjects() {
-        return constant;
-    }
+	private boolean intersect(GameObject o1, GameObject o2) {
+		return (Math.abs((o1.getX() + o1.getWidth() / 2) - (o2.getX() + o2.getWidth() / 2)) <= o1.getWidth())
+				&& (Math.abs((o1.getY() + o1.getHeight() / 2) - (o2.getY() + o2.getHeight() / 2)) <= o1.getHeight());
+	}
 
-    @Override
-    public List<GameObject> getMovableObjects() {
-        return moving;
-    }
+	@Override
+	public List<GameObject> getConstantObjects() {
+		return constant;
+	}
 
-    @Override
-    public List<GameObject> getControlableObjects() {
-        return control;
-    }
+	@Override
+	public List<GameObject> getMovableObjects() {
+		return moving;
+	}
 
-    @Override
-    public int getWidth() {
-        return width;
-    }
+	@Override
+	public List<GameObject> getControlableObjects() {
+		return control;
+	}
 
-    @Override
-    public int getHeight() {
-        return height;
-    }
+	@Override
+	public int getWidth() {
+		return width;
+	}
 
-    @Override
-    public boolean refresh() {
-        boolean timeout = System.currentTimeMillis() - startTime > MAX_TIME; // time end and game over
+	@Override
+	public int getHeight() {
+		return height;
+	}
 
-        // get Clown from here
-        ImageObject fighter = (ImageObject)control.get(0);
+	@Override
+	public boolean refresh() {
+		boolean timeout = System.currentTimeMillis() - startTime > MAX_TIME; // time end and game over
 
-        // handle moving plates here
-        for(GameObject o : moving.toArray(new GameObject[moving.size()])){
-                o.setY((o.getY() + 1));
-                if(o.getY()>=getHeight()){
-                    // reuse the plate in another position
-                    o.setY(-1 * (int)(Math.random() * getHeight()));
-                    o.setX((int)(Math.random() * getWidth()));
-                }
-                o.setX(o.getX() + (Math.random() > 0.5 ? 2 : -2));
-                if(!timeout & o.isVisible() && intersect(o, fighter)) {
-                    // clown caught a plate here
-                }
-            }
+		// get Clown from here
+		ImageObject fighter = (ImageObject) control.get(0);
 
-        return !timeout;
-    }
+		// handle moving plates here
+		for (GameObject o : moving.toArray(new GameObject[moving.size()])) {
+			o.setY((o.getY() + 1));
+			if (o.getY() >= getHeight()) {
+				// reuse the plate in another position
+				o.setY(-1 * (int) (Math.random() * getHeight()));
+				o.setX((int) (Math.random() * getWidth()));
+			}
+			o.setX(o.getX() + (Math.random() > 0.5 ? 2 : -2));
+			if (!timeout & o.isVisible() && intersect(o, fighter)) {
+				
+				// clown caught a plate here
+			}
+		}
 
-    private boolean intersect(GameObject o1, GameObject o2){
-        return false;
-    }
+		return !timeout;
+	}
 
-    @Override
-    public String getStatus() {
-        return "Score=" + score + "   |   Time=" + Math.max(0, (MAX_TIME - (System.currentTimeMillis()-startTime))/1000);	// update status
-    }
+	@Override
+	public String getStatus() {
+		return "Score=" + score + "   |   Time="
+				+ Math.max(0, (MAX_TIME - (System.currentTimeMillis() - startTime)) / 1000); // update status
+	}
 
-    @Override
-    public int getSpeed() {
-        return difficulty.speed();
-    }
+	@Override
+	public int getSpeed() {
+		return difficulty.speed();
+	}
 
-    @Override
-    public int getControlSpeed() {
-        return 20;
-    }
+	@Override
+	public int getControlSpeed() {
+		return 20;
+	}
 }
